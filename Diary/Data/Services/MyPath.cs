d58e705f1c274d6129dfc55311a0e7d8ccee09f4
Artem <artem.egorov.art2006@gmail.com>
2023-01-24 20:29:46 +0300

    Реализовал PathUSer

--- Diary/Data/Services/MyPath.cs
+++ Diary/Data/Services/MyPath.cs
@@ -10,7 +10,7 @@ namespace Diary.Data.Services
     {
         public string PathDirectory { get; protected set; }
         public string PathFile { get; protected set; }
-        public virtual void CreateNewPath(string name) { }
-        public virtual void CreateNewDirectory(string name) { }
+        public virtual void CreateNewPath(string nameFile) { }
+        public virtual void CreateNewDirectory(string nameDirectory) { }
     }
 }
